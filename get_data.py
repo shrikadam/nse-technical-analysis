ccbcc4607ea08466e759397bff5b1bc45296e2e6
Shriniwas <shriniwaskadam25@gmail.com>
2025-03-18 09:46:32 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -5,12 +5,12 @@ from tvDatafeed import TvDatafeed, Interval
 
 tv = TvDatafeed()
 
-nifty50_stocks_df = pd.read_csv("data/ind_nifty50list.csv")    
+nifty50_df = pd.read_csv("data/ind_nifty50list.csv")    
 # Create an empty dictionary to store the industry-wise stocks
 industry_dict = {}
 
 # Iterate over each row in the DataFrame
-for index, row in nifty50_stocks_df.iterrows():
+for index, row in nifty50_df.iterrows():
     industry = row['Industry']
     symbol = row['Symbol']
     
@@ -26,38 +26,34 @@ for index, row in nifty50_stocks_df.iterrows():
 #     print(f"Industry: {industry}")
 #     print(f"Stocks: {', '.join(symbols)}\n")
 
-# Function to fetch and plot data for each industry
-def fetch_and_plot_data(industry_dict):
-    for industry, symbols in industry_dict.items():
-        plt.figure(figsize=(12, 6))  # Create a new figure for each industry
-        for symbol in symbols:
-            # Fetch historical data for each stock
-            try:
-                df = tv.get_hist(symbol=symbol, exchange='NSE', interval=Interval.in_daily, n_bars=500)
-                # Normalize
-                df['close'] /= df['close'].iloc[0]
-                # Plot the close prices for each stock
-                plt.plot(df.index, df['close'], label=symbol)
-            except Exception as e:
-                print(f"Error fetching data for {symbol}: {e}")
-        
-        # Add legend and labels
-        plt.title(f"{industry} Stocks - Historical Data")
-        plt.xlabel("Date")
-        plt.ylabel("Close Price")
-        plt.legend(title="Stocks", loc="upper left")
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-%Y'))
-        plt.grid()
+nifty50_stocks = []
+n_bars = 500
+for industry, symbols in industry_dict.items():
+    plt.figure(figsize=(12, 6))  # Create a new figure for each industry
+    for symbol in symbols:
+        # Fetch historical data for each stock
+        try:
+            df = tv.get_hist(symbol=symbol, exchange='NSE', interval=Interval.in_daily, n_bars=n_bars)
+            df_ = df[['close']].rename(columns={'close': symbol})
+            nifty50_stocks.append(df_)
+            # Normalize
+            df['close'] /= df['close'].iloc[0]
+            # Plot the close prices for each stock
+            plt.plot(df.index, df['close'], label=symbol)
+        except Exception as e:
+            print(f"Error fetching data for {symbol}: {e}")
+    
+    # Add legend and labels
+    plt.title(f"{industry} Stocks - Historical Data")
+    plt.xlabel("Date")
+    plt.ylabel("Close Price")
+    plt.legend(title="Stocks", loc="upper left")
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-%Y'))
+    plt.grid()
 
-        # Save the figure with the industry name
-        plt.savefig(f"plots/{industry}_historical_data.png")
-        plt.close()  # Close the plot to avoid overlap in next iteration
+    # Save the figure with the industry name
+    plt.savefig(f"plots/{industry}_historical_data.png")
+    plt.close()  # Close the plot to avoid overlap in next iteration
 
-fetch_and_plot_data(industry_dict)
-
-# stock_data = tv.get_hist(symbol='BAJAJ_AUTO',exchange='NSE',interval=Interval.in_daily,n_bars=500)
-# print(stock_data.columns)
-# stock_data["close"].plot()
-# plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b-%Y'))
-# plt.grid()
-# plt.show()
+nifty50_stocks_df = pd.concat(nifty50_stocks, axis=1)
+nifty50_stocks_df.to_csv('nifty50_histdata.csv', index=True)
